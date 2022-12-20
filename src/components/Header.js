e7354f0c18696eee50321a05e36b5f74c27a00d5
Kate Danilenko <keitilins@yandex.ru>
2022-12-20 19:26:45 +0300

    feat: burger-menu для мобильной версии

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,45 @@
 import logo from '../images/logo.svg'
+import React, { useState } from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 
 function Header(props) {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  function handleBurgerClick() {
+    setMenuOpen((state) => !state);
+  }
+
   return (
     <header className="header">
-      <Link to="/">
-        <img src={logo} className="header__logo" alt="Логотип"/>
-      </Link>
-      <Switch>
-        <Route path="/sign-up">
-          <Link to="/sign-in" className="header__link">Войти</Link>
-        </Route>
-        <Route path="/sign-in">
-          <Link to="/sign-up" className="header__link">Регистрация</Link>
-        </Route>
-        <Route exact path="/">
-          <div className="header__infobox">
-            <p className="header__user-email">{props.userEmail}</p>
-            <Link to="/sign-in" className="header__link" onClick={props.onSignOut}>Выйти</Link>
-          </div>
-        </Route> 
-      </Switch> 
+      <div className={`header__menu ${isMenuOpen ? "header__menu_opened" : ""}`}>
+          <p className="header__user-email">{props.userEmail}</p>
+          <Link to="/sign-in" className="header__link" onClick={props.onSignOut}>Выйти</Link>
+      </div>
+      
+      <div className="header__container">
+        <Link to="/">
+          <img src={logo} className="header__logo" alt="Логотип"/>
+        </Link>
+        <Switch>
+          <Route path="/sign-up">
+            <Link to="/sign-in" className="header__link">Войти</Link>
+          </Route>
+          <Route path="/sign-in">
+            <Link to="/sign-up" className="header__link">Регистрация</Link>
+          </Route>
+          <Route exact path="/">
+            <div className="header__infobox">
+              <p className="header__user-email">{props.userEmail}</p>
+              <Link to="/sign-in" className="header__link" onClick={props.onSignOut}>Выйти</Link>
+            </div>
+            <div className={`header__burger-btn button ${isMenuOpen ? "header__burger-btn_transformed" : ""}`} onClick={handleBurgerClick}>
+              <span className="header__burger-layer"></span>
+              <span className="header__burger-layer"></span>
+              <span className="header__burger-layer"></span>
+            </div>
+          </Route> 
+        </Switch> 
+      </div>
     </header>
   )
 }
